8e094c7da49afc63cb9e2173a234e73877e7322c
euktop <eugenij.trun@yandex.ru>
2026-04-27 22:35:12 +0500

    Навигация работает

--- app/src/main/java/stud/euktop/schooljournal/presentation/common/navigate/impl/RouterImpl.kt
+++ app/src/main/java/stud/euktop/schooljournal/presentation/common/navigate/impl/RouterImpl.kt
@@ -1,9 +1,22 @@
 package stud.euktop.schooljournal.presentation.common.navigate.impl
 
+import stud.euktop.schooljournal.R
+import stud.euktop.schooljournal.presentation.common.navigate.NavCommand
+import stud.euktop.schooljournal.presentation.common.navigate.contract.NavigationManager
 import stud.euktop.schooljournal.presentation.common.navigate.contract.RouterSplash
 import javax.inject.Inject
 
-class RouterImpl @Inject constructor() : RouterSplash {
+class RouterImpl @Inject constructor(
+    private val navigationManager: NavigationManager
+) : RouterSplash {
     override suspend fun navigateAction() {
+        navigationManager.navigate(
+            NavCommand.ToDestination(
+                destId = R.id.nav_main_main,
+                targetKey = null,
+                popUpTo = R.id.splashFragment,
+                inclusive = true
+            )
+        )
     }
 }
